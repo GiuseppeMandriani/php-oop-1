a01584ce277b0c3ab3a34dc6094f9f9e6478bfb2
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-31 16:23:05 +0200

    add fake DB e new Index

--- index.php
+++ index.php
@@ -1,97 +1,50 @@
-<?php
-/**
- *  MOVIE CLASS     (Modello degli oggetti)
- *  OBJECT -> Istanza di una classe
- */
+<?php // Import Class Movie e Data Fake per Connessione al DB
+    require_once __DIR__ . '/movie.php';
+    require_once __DIR__ . '/movie_data.php';
+?>
 
-class Movie{
+<!DOCTYPE html>
+<html lang="en">
 
-    /**
-     *  Attributi/Proprietà/Variabili
-     */
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <!-- FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <!-- CSS Style -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>DB OOP</title>
+</head>
 
-    public $title;
-    public $genre;
-    public $date_of_release;
-    public $director;
-    public $language;
-    // public $review = 'nd';
-    public $summary;
+<body>
+    <header>
+        <h1>DB OOP</h1>
+    </header>
 
-    // Constructor
+    <main>
+        <section>
+            <?php // Movie List 
+            foreach ($movies as $movie) {
+                // Creo Istanze
+                $film = new Movie($movie['title'], $movie['genre'], $movie['review'] = 'nd', $movie['date_of_release'], $movie['director'], $movie['language'], $movie['summary']); ?>
+                <article>
+                    <img src="<?php echo $movie['url']; ?>" alt="">
+                    <h2><?php echo $film->title; ?></h2>
+                    <p><?php echo $film->genre; ?></p>
+                    <p><?php echo $film->review; ?></p>
+                    <p><?php echo $film->date_of_release; ?></p>
+                    <p><?php echo $film->director; ?></p>
+                    <p><?php echo $film->language; ?></p>
+                    <p><?php echo $film->getExcepert().'...'; ?></p>
+                </article>
 
-    function __construct($_title,$_genre,$_review = 'nd'){
-        $this->title = $_title;
-        $this->genre = $_genre;
-        $this->review = $_review;
-    }
+            <?php } ?>
+        </section>
 
+    </main>
 
-    // Methods
+</body>
 
-    /**
-     * Set Date of Release
-     */
-    
-    public function setDateOfRelease($date_of_release){
-        $this->date_of_release = $date_of_release;
-    }
-
-    /**
-     * GET EXCEPERT
-     */
-
-    public function getExcepert(){
-        $sub = substr($this->summary, 0, 80,);
-        return $sub;
-    }
-}
-
-// Istanza Primo Film
-$film_1 = new Movie('Crudelia','Fantasy');
-// $film_1->title = 'Crudelia';
-// $film_1->genre = 'Fantasy';
-$film_1->setDateOfRelease(2021);
-$film_1->director = 'Craig Gillepsie';
-$film_1->language = 'EN';
-$film_1->summary = "Crudelia, il film diretto da Craig Gillespie, vede protagonista Emma Stone nei panni di una giovane Crudelia De Mon. L'origin story, in live action, racconta la nascita e la trasformazione dell'iconico e malvagio personaggio de La carica dei 101.
-Estella De Vil (nome di origine di Cruella) è una giovane truffatrice dai capelli rosso scuro, una ragazza brillante e ambiziosa, decisa a sfondare nel mondo della moda come fashion designer, in una Londra influenzata dalla rivoluzione punk-rock dei primi anni '70. La ragazza si avventura per le strade della città in compagnia di due ladri dilettanti che adorano il suo lato malvagio.
-Molto presto Estella viene notata dalla baronessa von Hellman (Emma Thompson), direttrice di una prestigiosa casa di moda, una donna straordinariamente elegante e raffinata. L'incontro tra le due tuttavia darà luogo a una serie di eventi in cui il lato oscuro, cattivo e vendicativo di Estella prenderà il sopravvento trasformandola nella Cruella che tutti conosciamo.";
-
-echo '<p>' .$film_1->getExcepert() . '</p>';
-var_dump($film_1);
-
-// Istanza Secondo Film
-$film_2 = new Movie('Aladdin','Fantasy');
-// $film_2->title = 'Aladdin';
-// $film_2->genre = 'Fantasy';
-$film_2->setDateOfRelease(2019);
-$film_2->director = 'Guy Ritchie';
-$film_2->language = 'EN';
-$film_2->summary = "Aladdin è un ragazzo poverissimo che campa di espedienti nella città di Agrabah. Durante un furtarello incontra la principessa Jasmine, cui è stato proibito di uscire dal palazzo reale: ma Jasmine vuole conoscere la sua città, e va in giro spacciandosi per la sua ancella Dalia. Quando Aladdin scopre la vera identità della principessa è troppo tardi: ne è già innamorato, ma sa di essere troppo 'straccione' per aspirare alla sua mano. Il perfido visir Jafar, stanco del ruolo di eterno secondo e desideroso di impossessarsi del trono del sultano, spedisce Aladdin a rubare una lampada magica che rende chi la possiede potentissimo. Ma dentro la lampada c'è un genio e Jafar ha progetti pericolosi: dunque Aladdin si terrà stretti lampada e genio, con l'aiuto della scimmietta Abu e di un velocissimo tappeto magico." ;
-
-echo '<p>' .$film_2->getExcepert() . '</p>';
-
-
-var_dump($film_2);
-
-// Istanza Terzo Film
-$film_3 = new Movie('Thor', 'Fantasy-Action',5);
-$film_3->title = 'Thor';
-$film_3->genre = 'Fantasy-Action';
-$film_3->setDateOfRelease(2011);
-
-// $film_3->date_of_release = '2011';
-$film_3->director = 'Kenneth Branagh';
-$film_3->language = 'EN';
-$film_3->summary = "Dopo che le sue azioni sconsiderate hanno riacceso un'antica guerra, il dio nordico Thor viene spogliato dei propri poteri, scacciato dal regno di Asgard, e costretto a vivere tra gli umani." ;
-
-echo '<p>' .$film_3->getExcepert() . '</p>';
-
-var_dump($film_3);
-
-
-// CLASS UTILITIES
-var_dump($film_1 instanceof Movie);      // True o False
-
-var_dump(get_class($film_1)); // Ritorna stringa della classe
+</html>
---
+++ movie_data.php
@@ -0,0 +1,46 @@
+<?php // Emulo DB MOVIE
+
+$movies = [
+    [
+        'title' => 'Crudelia',
+        'genre' => 'Fantasy',
+        'date_of_release' => 2021,
+        'director' => 'Craig Gillepsie',
+        'language' => 'EN',
+        'url' => 'https://picsum.photos/200',
+        // 'review' => '',
+
+        'summary' => 'Crudelia, il film diretto da Craig Gillespie, vede protagonista Emma Stone nei panni di una giovane Crudelia De Mon. L\'origin story, in live action, racconta la nascita e la trasformazione dell\'iconico e malvagio personaggio de La carica dei 101. <br><br>
+
+        Estella De Vil (nome di origine di Cruella) è una giovane truffatrice dai capelli rosso scuro, una ragazza brillante e ambiziosa, decisa a sfondare nel mondo della moda come fashion designer, in una Londra influenzata dalla rivoluzione punk-rock dei primi anni \'70. La ragazza si avventura per le strade della città in compagnia di due ladri dilettanti che adorano il suo lato malvagio. <br><br>
+
+        Molto presto Estella viene notata dalla baronessa von Hellman (Emma Thompson), direttrice di una prestigiosa casa di moda, una donna straordinariamente elegante e raffinata. L\'incontro tra le due tuttavia darà luogo a una serie di eventi in cui il lato oscuro, cattivo e vendicativo di Estella prenderà il sopravvento trasformandola nella Cruella che tutti conosciamo.',
+    ],
+    [
+        'title' => 'Aladdin',
+        'genre' => 'Fantasy',
+        'date_of_release' => 2019,
+        'director' => 'Guy Ritchie',
+        'language' => 'EN',
+        'url' => 'https://picsum.photos/200',
+
+        'review' => '',
+        'summary' => 'Aladdin è un ragazzo poverissimo che campa di espedienti nella città di Agrabah. Durante un furtarello incontra la principessa Jasmine, cui è stato proibito di uscire dal palazzo reale: ma Jasmine vuole conoscere la sua città, e va in giro spacciandosi per la sua ancella Dalia. <br><br>
+        
+        Quando Aladdin scopre la vera identità della principessa è troppo tardi: ne è già innamorato, ma sa di essere troppo \'straccione\' per aspirare alla sua mano. Il perfido visir Jafar, stanco del ruolo di eterno secondo e desideroso di impossessarsi del trono del sultano, spedisce Aladdin a rubare una lampada magica che rende chi la possiede potentissimo. Ma dentro la lampada c\'è un genio e Jafar ha progetti pericolosi: dunque Aladdin si terrà stretti lampada e genio, con l\'aiuto della scimmietta Abu e di un velocissimo tappeto magico.',
+    ],
+    [
+        'title' => 'Thor',
+        'genre' => 'Fantasy-Action',
+        'date_of_release' => 2011,
+        'director' => 'Kenneth Branagh',
+        'language' => 'EN',
+        'url' => 'https://picsum.photos/200',
+
+        // 'review' => 5,
+
+        'summary' => "Dopo che le sue azioni sconsiderate hanno riacceso un'antica guerra, il dio nordico Thor viene spogliato dei propri poteri, scacciato dal regno di Asgard, e costretto a vivere tra gli umani.",
+    ],
+];
+
+// var_dump($movies);
